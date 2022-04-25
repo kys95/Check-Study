d27c29792fdef78a9816daec0286c16069b225dc
Yunseok Kang <mok02198@gmail.com>
2022-04-25 23:34:26 +0900

    Initial commit

---
+++ src/main/java/com/kys95/checkstudy/CheckstudyApplication.java
@@ -0,0 +1,13 @@
+package com.kys95.checkstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kys95/checkstudy/CheckstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kys95.checkstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
