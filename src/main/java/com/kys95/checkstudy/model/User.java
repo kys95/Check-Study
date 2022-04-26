ab0d6f43412b3e7f1a5c584d14918cafa8190bb3
Yunseok Kang <mok02198@gmail.com>
2022-04-26 23:55:55 +0900

    Create User.java

---
+++ src/main/java/com/kys95/checkstudy/model/User.java
@@ -0,0 +1,37 @@
+package com.kys95.checkstudy.model;
+
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@NoArgsConstructor
+@Getter
+@Builder
+@Entity
+public class User  extends BaseTimeEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String nickname;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+}
