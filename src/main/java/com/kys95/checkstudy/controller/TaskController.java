f7561cdbd05e02b179fed27173747688e3ab5ded
Yunseok Kang <mok02198@gmail.com>
2022-05-19 23:55:03 +0900

    Update TaskController.java

--- src/main/java/com/kys95/checkstudy/controller/TaskController.java
+++ src/main/java/com/kys95/checkstudy/controller/TaskController.java
@@ -1,13 +1,29 @@
 package com.kys95.checkstudy.controller;
 
+import com.kys95.checkstudy.model.Task;
+import com.kys95.checkstudy.service.TaskService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final TaskService taskService;
 
     @GetMapping("/taskForm")
     public String taskForm(){
         return "task/taskForm";
+    }
+
+    @GetMapping("/taskView")
+    public String taskView(Model model){
+        List<Task> tasks = taskService.findList();
+        model.addAttribute("tasks",tasks);
+        return "task/taskView";
     }
 }
