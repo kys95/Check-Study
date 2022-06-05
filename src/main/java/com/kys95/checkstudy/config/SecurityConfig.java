d1a113d5b6907af7ffea2856b7d1f55f63dfb717
Yunseok Kang <mok02198@gmail.com>
2022-06-05 23:07:34 +0900

    Create SecurityConfig.java

---
+++ src/main/java/com/kys95/checkstudy/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.kys95.checkstudy.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder pwdEncode(){
+        return new BCryptPasswordEncoder();
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/loginForm")
+                .loginProcessingUrl("/api/loginProc")
+                .defaultSuccessUrl("/");
+    }
+}
