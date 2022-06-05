4027c37e01f96cd020e03dda6101941ac15eb8ec
Yunseok Kang <mok02198@gmail.com>
2022-06-05 23:08:12 +0900

    Update UserService.java

--- src/main/java/com/kys95/checkstudy/service/UserService.java
+++ src/main/java/com/kys95/checkstudy/service/UserService.java
@@ -1,8 +1,10 @@
 package com.kys95.checkstudy.service;
 
 import com.kys95.checkstudy.dto.UserJoinDto;
+import com.kys95.checkstudy.model.Role;
 import com.kys95.checkstudy.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,10 +13,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Transactional
     public void join(UserJoinDto userJoinDto){
-
+        String rawPassword = userJoinDto.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        userJoinDto.setPassword(encPassword);
+        userJoinDto.setRole(Role.USER);
         userRepository.save(userJoinDto.toEntity());
     }
 
