aa9b4481dccfb23189ff14a5a4c165bafb73a892
Yunseok Kang <mok02198@gmail.com>
2022-06-06 23:22:58 +0900

    Update IndexController.java

--- src/main/java/com/kys95/checkstudy/controller/IndexController.java
+++ src/main/java/com/kys95/checkstudy/controller/IndexController.java
@@ -1,13 +1,23 @@
 package com.kys95.checkstudy.controller;
 
+import com.kys95.checkstudy.config.auth.PrincipalDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping({"","/"})
-    public String index(){
+    public String index(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        if (principalDetails != null) {
+            String username = principalDetails.getUsername();
+            Long userId = principalDetails.getUserId();
+            model.addAttribute("username", username);
+            model.addAttribute("userId", userId);
+        }
         return "index";
+
     }
 }
