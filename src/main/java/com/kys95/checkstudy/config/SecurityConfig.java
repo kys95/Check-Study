5c840c06bfe88a95eb43922e53c0a8d51d0b1107
Yunseok Kang <mok02198@gmail.com>
2022-06-08 23:45:33 +0900

    Update SecurityConfig.java

--- src/main/java/com/kys95/checkstudy/config/SecurityConfig.java
+++ src/main/java/com/kys95/checkstudy/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.kys95.checkstudy.config;
 
+import com.kys95.checkstudy.config.auth.PrincipalDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,10 +15,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PrincipalDetailsService principalDetailsService;
+
     @Bean
     public BCryptPasswordEncoder pwdEncode(){
         return new BCryptPasswordEncoder();
     };
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(principalDetailsService).passwordEncoder(pwdEncode());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
