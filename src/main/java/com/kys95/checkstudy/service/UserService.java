751933e796a00a2860023fb10bfd7b771177cbe0
Yunseok Kang <mok02198@gmail.com>
2022-06-09 22:16:42 +0900

    Update UserService.java

--- src/main/java/com/kys95/checkstudy/service/UserService.java
+++ src/main/java/com/kys95/checkstudy/service/UserService.java
@@ -1,7 +1,9 @@
 package com.kys95.checkstudy.service;
 
 import com.kys95.checkstudy.dto.UserJoinDto;
+import com.kys95.checkstudy.dto.UserUpdateDto;
 import com.kys95.checkstudy.model.Role;
+import com.kys95.checkstudy.model.User;
 import com.kys95.checkstudy.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,4 +26,13 @@ public class UserService {
         userRepository.save(userJoinDto.toEntity());
     }
 
+    @Transactional
+    public void update(Long userId, UserUpdateDto userUpdateDto) {
+        User findUser = userRepository.findById(userId).orElseThrow(()->{
+            return new IllegalArgumentException("해당 유저를 찾을 수 없습니다.");
+        });
+        String rawPassword = userUpdateDto.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        findUser.update(encPassword, userUpdateDto.getNickname(),userUpdateDto.getEmail());
+    }
 }
