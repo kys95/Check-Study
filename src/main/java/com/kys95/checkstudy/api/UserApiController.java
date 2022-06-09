19cd3ebbff49af4f16df0a2ee2a428c6095c2ac8
Yunseok Kang <mok02198@gmail.com>
2022-06-09 22:15:46 +0900

    Update UserApiController.java

--- src/main/java/com/kys95/checkstudy/api/UserApiController.java
+++ src/main/java/com/kys95/checkstudy/api/UserApiController.java
@@ -1,12 +1,11 @@
 package com.kys95.checkstudy.api;
 
 import com.kys95.checkstudy.dto.UserJoinDto;
+import com.kys95.checkstudy.dto.UserUpdateDto;
 import com.kys95.checkstudy.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +16,12 @@ public class UserApiController {
     @PostMapping("/api/joinProc")
     public int joinProc(@RequestBody UserJoinDto userJoinDto){
         userService.join(userJoinDto);
+        return HttpStatus.OK.value();
+    }
+
+    @PutMapping("/api/userProc/{userId}")
+    public int update(@RequestBody UserUpdateDto userUpdateDto, @PathVariable Long userId){
+        userService.update(userId, userUpdateDto);
         return HttpStatus.OK.value();
     }
 }
