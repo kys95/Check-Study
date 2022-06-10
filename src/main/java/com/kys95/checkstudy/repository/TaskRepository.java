db26322140c36ffe6a255bc4f42a7853864f5278
Yunseok Kang <mok02198@gmail.com>
2022-06-10 23:45:16 +0900

    Update TaskRepository.java

--- src/main/java/com/kys95/checkstudy/repository/TaskRepository.java
+++ src/main/java/com/kys95/checkstudy/repository/TaskRepository.java
@@ -3,5 +3,8 @@ package com.kys95.checkstudy.repository;
 import com.kys95.checkstudy.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findAllByUserId(Long userId);
 }
