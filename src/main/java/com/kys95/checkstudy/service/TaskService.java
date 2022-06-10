dbe645b6da579a5b884905d94bd3298a31adcc03
Yunseok Kang <mok02198@gmail.com>
2022-06-10 23:45:26 +0900

    Update TaskService.java

--- src/main/java/com/kys95/checkstudy/service/TaskService.java
+++ src/main/java/com/kys95/checkstudy/service/TaskService.java
@@ -1,8 +1,11 @@
 package com.kys95.checkstudy.service;
 
+import com.kys95.checkstudy.config.auth.PrincipalDetails;
 import com.kys95.checkstudy.dto.TaskWriteDto;
 import com.kys95.checkstudy.model.Task;
+import com.kys95.checkstudy.model.User;
 import com.kys95.checkstudy.repository.TaskRepository;
+import com.kys95.checkstudy.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,15 +17,20 @@ import java.util.List;
 public class TaskService {
 
     private final TaskRepository taskRepository;
+    private final UserRepository userRepository;
 
     @Transactional
-    public void write(TaskWriteDto taskWriteDto){
+    public void write(TaskWriteDto taskWriteDto, PrincipalDetails principalDetails){
+        User findUser = userRepository.findByUsername(principalDetails.getUsername()).orElseThrow(()->{
+            return new IllegalArgumentException("해당 유저를 찾을 수 없습니다.");
+        });
+        taskWriteDto.setUser(findUser);
         taskRepository.save(taskWriteDto.toEntity());
     }
 
     @Transactional(readOnly = true)
-    public List<Task> findList() {
-        return taskRepository.findAll();
+    public List<Task> findList(PrincipalDetails principalDetails) {
+        return taskRepository.findAllByUserId(principalDetails.getUserId());
     }
 
 }
