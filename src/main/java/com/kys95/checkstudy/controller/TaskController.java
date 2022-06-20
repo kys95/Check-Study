e607130082b22df396b07a218db79e5a05b94832
Yunseok Kang <mok02198@gmail.com>
2022-06-20 23:41:42 +0900

    Update TaskController.java

--- src/main/java/com/kys95/checkstudy/controller/TaskController.java
+++ src/main/java/com/kys95/checkstudy/controller/TaskController.java
@@ -4,13 +4,20 @@ import com.kys95.checkstudy.config.auth.PrincipalDetails;
 import com.kys95.checkstudy.model.Task;
 import com.kys95.checkstudy.service.TaskService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.time.format.DateTimeFormatter;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -21,19 +28,29 @@ public class TaskController {
     @GetMapping("/taskForm")
     public String taskForm(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails){
         String username = principalDetails.getUsername();
-        model.addAttribute("username",principalDetails);
-        model.addAttribute("now", LocalDateTime.now());
+        model.addAttribute("username",username);
         return "task/taskForm";
     }
 
-    @GetMapping("/todolist/proceeding")
-    public String taskView(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails){
-        List<Task> tasks = taskService.findList(principalDetails);
-        String username =principalDetails.getUsername();
-        String nickname = principalDetails.getNickname();
-        model.addAttribute("tasks",tasks);
-        model.addAttribute("username",username);
-        model.addAttribute("nickname",nickname);
+    @ResponseBody
+    public String taskView(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails,
+                               @PageableDefault(size=8, sort = "deadline", direction = Sort.Direction.ASC) Pageable pageable, int page) {
+        Page<Task> pagingTasks  = taskService.findList(principalDetails, pageable);
+        int nextPage = pagingTasks.getNumber()+1;
+        model.addAttribute("username", principalDetails);
+        model.addAttribute("nextPage", nextPage);
+        model.addAttribute("pagingTasks",pagingTasks);
         return "task/taskView";
+    }
+
+    @GetMapping("/task/detail/{id}")
+    public String taskDetail(@PathVariable long id, Model model, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        Task requestTask = taskService.findTask(id);
+        String username = principalDetails.getUsername();
+        model.addAttribute("task",requestTask);
+        model.addAttribute("username",username);
+        String deadline = requestTask.getDeadline().format(DateTimeFormatter.ofPattern("yyyy/MM/dd hh:mm"));
+        model.addAttribute("deadline",deadline);
+        return "task/taskDetail";
     }
 }
