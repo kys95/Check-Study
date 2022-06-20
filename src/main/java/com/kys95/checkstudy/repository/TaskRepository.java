132dbd3e059eeeb35f88d3dc620163854b623fc2
Yunseok Kang <mok02198@gmail.com>
2022-06-20 23:42:07 +0900

    Update TaskRepository.java

--- src/main/java/com/kys95/checkstudy/repository/TaskRepository.java
+++ src/main/java/com/kys95/checkstudy/repository/TaskRepository.java
@@ -1,10 +1,12 @@
 package com.kys95.checkstudy.repository;
 
 import com.kys95.checkstudy.model.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    List<Task> findAllByUserIdAndIsSuccess(Long userId, int isSuccess);
+    Page<Task> findAllByUserIdAndIsSuccess(Long userId, int isSuccess, Pageable pageable);
 }
