8c5259a159c44356271599013d973a70eb8ef15e
Yunseok Kang <mok02198@gmail.com>
2022-06-20 23:44:10 +0900

    Create FeedbackRepository.java

---
+++ src/main/java/com/kys95/checkstudy/repository/FeedbackRepository.java
@@ -0,0 +1,6 @@
+package com.kys95.checkstudy.repository;
+
+import com.kys95.checkstudy.model.Feedback;
+
+public interface FeedbackRepository extends org.springframework.data.jpa.repository.JpaRepository<Feedback, Long> {
+}
