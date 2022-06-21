2a1676d1a6eae43cc50fb516f4c5a0f9b1e09080
Yunseok Kang <mok02198@gmail.com>
2022-06-21 23:54:39 +0900

    Update TaskApiController.java

--- src/main/java/com/kys95/checkstudy/api/TaskApiController.java
+++ src/main/java/com/kys95/checkstudy/api/TaskApiController.java
@@ -1,14 +1,13 @@
 package com.kys95.checkstudy.api;
 
 import com.kys95.checkstudy.config.auth.PrincipalDetails;
+import com.kys95.checkstudy.dto.TaskUpdateDto;
 import com.kys95.checkstudy.dto.TaskWriteDto;
 import com.kys95.checkstudy.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +18,18 @@ public class TaskApiController {
     @PostMapping("api/writeProc")
     public int write(@RequestBody TaskWriteDto taskWriteDto, @AuthenticationPrincipal PrincipalDetails principalDetails){
         taskService.write(taskWriteDto, principalDetails);
+        return HttpStatus.OK.value();
+    }
+
+    @DeleteMapping("/api/taskDelete/{id}")
+    public int delete(@PathVariable long id){
+        taskService.delete(id);
+        return HttpStatus.OK.value();
+    }
+
+    @PutMapping("/api/taskUpdate/{id}")
+    public int update(@PathVariable long id, @AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody TaskUpdateDto taskUpdateDto){
+        taskService.update(id, taskUpdateDto);
         return HttpStatus.OK.value();
     }
 }
