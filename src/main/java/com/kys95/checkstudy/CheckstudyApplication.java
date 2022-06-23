2ad2160c06313bc6c15c1df1cb1fca89b3405c10
Yunseok Kang <mok02198@gmail.com>
2022-06-23 23:30:16 +0900

    Update CheckstudyApplication.java

--- src/main/java/com/kys95/checkstudy/CheckstudyApplication.java
+++ src/main/java/com/kys95/checkstudy/CheckstudyApplication.java
@@ -2,7 +2,9 @@ package com.kys95.checkstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CheckstudyApplication {
 
