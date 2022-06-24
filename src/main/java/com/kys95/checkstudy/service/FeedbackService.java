5ace68e7be1a9b2bef13060fd44db31e93bb66f6
Yunseok Kang <mok02198@gmail.com>
2022-06-24 23:26:55 +0900

    Create FeedbackService.java

---
+++ src/main/java/com/kys95/checkstudy/service/FeedbackService.java
@@ -0,0 +1,16 @@
+package com.kys95.checkstudy.service;
+
+import com.kys95.checkstudy.dto.FeedbackCreateDto;
+import com.kys95.checkstudy.repository.FeedbackRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class FeedbackService {
+    private final FeedbackRepository feedbackRepository;
+
+    public void createFeedback(FeedbackCreateDto feedbackCreateDto) {
+        feedbackRepository.createReply(feedbackCreateDto.getUserId(), feedbackCreateDto.getUserId(), feedbackCreateDto.getContent());
+    }
+}
