0695d77f85b4be133dc70fc4953c2ee2746c2a2d
Yunseok Kang <mok02198@gmail.com>
2022-06-28 00:46:28 +0900

    Update user.js

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -9,7 +9,6 @@ let index ={
     },
 
     update: function (){
-
         let data={
             userId:parseInt($("#userId").val(),10),
             nickname: $("#nickname").val(),
@@ -20,19 +19,19 @@ let index ={
             alert("공백인 칸이 존재합니다.");
             return;
         }
-        $.ajax({
-            type:"PUT",
-            url:"/api/userProc/"+data.userId,
-            contentType: "application/json;charset=utf-8",
-            data: JSON.stringify(data)
+       $.ajax({
+           type:"PUT",
+           url:"/api/userProc/"+data.userId,
+           contentType: "application/json;charset=utf-8",
+           data: JSON.stringify(data)
             })
-            .done(function (response){
-                alert("회원 수정이 완료되었습니다.");
-                window.history.back();
-            })
-            .fail(function (error){
-                alert(JSON.stringify(error));
-            });
+           .done(function (response){
+               alert("회원 수정이 완료되었습니다.");
+               window.history.back();
+           })
+           .fail(function (error){
+               alert(JSON.stringify(error));
+           });
     },
 
     save : function(){
@@ -42,30 +41,27 @@ let index ={
             email:$("#email").val(),
             nickname:$("#nickname").val(),
         };
-
         if(data.password=="" || data.nickname=="" || data.email=="" || data.username ==""){
             alert("공백인 칸이 존재합니다.");
             return;
         }
 
-
         $.ajax({
             type:"POST",
             url:"/api/joinProc",
             data:JSON.stringify(data),
-            dataType:"application/json;charset=utf-8"
             contentType:"application/json; charset=utf-8"
         })
         .done(function(response){
             if(response.status === 500){
-                alert("회원가입에 실패하였습니다.");
+                alert("회원 가입에 실패하였습니다.");
             }
             else if(response==false){
                 alert("중복된 아이디입니다.");
                 return;
             }
             else{
-                alert("회원가입에 성공하였습니다.");
+                alert("회원 가입에 성공하였습니다.");
             }
             location.href="/";
         }).fail(function (error){
