dccfb6d394647f1a4a515cc5ebe3fe2ccaf43f17
Yunseok Kang <mok02198@gmail.com>
2022-06-28 00:44:17 +0900

    Update FeedbackService.java

--- src/main/java/com/kys95/checkstudy/service/FeedbackService.java
+++ src/main/java/com/kys95/checkstudy/service/FeedbackService.java
@@ -5,15 +5,19 @@ import com.kys95.checkstudy.repository.FeedbackRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @RequiredArgsConstructor
 @Service
 public class FeedbackService {
     private final FeedbackRepository feedbackRepository;
 
+    @Transactional
     public void createFeedback(FeedbackCreateDto feedbackCreateDto) {
         feedbackRepository.createReply(feedbackCreateDto.getUserId(), feedbackCreateDto.getTaskId(), feedbackCreateDto.getContent());
     }
 
+    @Transactional
     public void deleteFeedback(long id) {
         feedbackRepository.deleteById(id);
     }
