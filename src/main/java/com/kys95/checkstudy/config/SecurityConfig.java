2223d4cb83ae23cdfcf860474d949b08ff3b21e9
Yunseok Kang <mok02198@gmail.com>
2022-06-28 00:45:11 +0900

    Update SecurityConfig.java

--- src/main/java/com/kys95/checkstudy/config/SecurityConfig.java
+++ src/main/java/com/kys95/checkstudy/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
+                .antMatchers("/userInfo","/todolist/**").authenticated()
                 .anyRequest()
                 .permitAll()
                 .and()
