af319d54e15f69b948489554aca278d6ea1a5d8b
Yunseok Kang <mok02198@gmail.com>
2022-06-29 22:26:54 +0900

    Update TaskController.java

--- src/main/java/com/kys95/checkstudy/controller/TaskController.java
+++ src/main/java/com/kys95/checkstudy/controller/TaskController.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 
 
@@ -52,6 +52,7 @@ public class TaskController {
         model.addAttribute("successPage",true);
         return "task/taskView";
     }
+
 
     @GetMapping("/task/detail/{id}")
     public String taskDetail(@PathVariable long id, Model model, @AuthenticationPrincipal PrincipalDetails principalDetails){
