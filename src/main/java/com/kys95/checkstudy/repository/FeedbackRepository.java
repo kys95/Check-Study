1094e8f14218e9fd6d21304dc0dec6a84897bc14
Yunseok Kang <mok02198@gmail.com>
2022-06-29 22:26:02 +0900

    Update FeedbackRepository.java

--- src/main/java/com/kys95/checkstudy/repository/FeedbackRepository.java
+++ src/main/java/com/kys95/checkstudy/repository/FeedbackRepository.java
@@ -1,12 +1,14 @@
 package com.kys95.checkstudy.repository;
 
 import com.kys95.checkstudy.model.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-public interface FeedbackRepository extends org.springframework.data.jpa.repository.JpaRepository<Feedback, Long> {
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+
 
     @Modifying
-    @Query(value = "INSERT INTO feedback(userId, taskId, content, createDate) VALUES(?1, ?2, ?3, now())",nativeQuery = true)
+    @Query(value = "INSERT INTO feedback(userId, taskId, content, createdDate) VALUES(?1, ?2, ?3, now())",nativeQuery = true)
     void createReply(long userId, long taskId, String content);
 }
